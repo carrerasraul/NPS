ffb17a3681e8f8c76e2efd82acbd6393ca6ab8f3
carrerasraul <52898376+carrerasraul@users.noreply.github.com>
2019-11-25 17:46:09 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,65 @@
+'use strict';
+
+function formatQuery(params) {
+    const queryItems = Object.keys(params).map(key => `${[encodeURIComponent(key)]}=${encodeURIComponent(params[key])}`);
+    return queryItems.join('&');
+}
+
+function displayResults(responseJson, maxResults) {
+    console.log(responseJson);
+
+    $('.js-error').empty();
+    $('.results-list').empty();
+
+    for (let i = 0; i < responseJson.data.length & i < maxResults; i++) {
+        $('.results-list').append(`<li><h3><a href="${responseJson.data[i].url}">${responseJson.data[i].fullName}</a></h3>
+        <p>${responseJson.data[i].description}</p>
+        </li>`);
+    }
+    $('.results').removeClass('hidden');
+}
+
+function getParks(baseUrl, stateArr, maxResults, apiKey) {
+
+    const params = {
+        stateCode: stateArr,
+        limit: maxResults
+    }
+
+    const queryString = formatQuery(params);
+    const url = baseUrl + '?' + queryString + '&api_key=' + apiKey;
+    console.log(url);
+
+
+    fetch(url)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => displayResults(responseJson, maxResults))
+        .catch(err => {
+            $('.js-error').text(`Something went wrong: ${err.message}`);
+        });
+}
+
+
+function watchForm() {
+    $('.js-form').on('submit', function() {
+        event.preventDefault();
+        const baseUrl = 'https://api.nps.gov/api/v1/parks'
+        const stateArr = $('.js-state-entered').val().split(",");
+        const maxResults = $('.js-result-amt').val();
+
+        const apiKey = '4zMvZfVgWpTYOlAmQi2FByezqsmaVCqhpfABuNgR'
+        getParks(baseUrl, stateArr, maxResults, apiKey);
+    })
+}
+
+$(function() {
+    console.log('App loaded! Waiting for submit!');
+    watchForm();
+});
+
+$(watchForm);
